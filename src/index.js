45f5eb0db5e8d1f1bb7e22768e6b0e491b734f2b
amazingfate <liujianfeng1994@gmail.com>
2024-07-08 11:58:51 +0800

    use my own domain

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.haguro.top": dockerHub,
+  "quay.haguro.top": "https://quay.io",
+  "gcr.haguro.top": "https://gcr.io",
+  "k8s-gcr.haguro.top": "https://k8s.gcr.io",
+  "k8s.haguro.top": "https://registry.k8s.io",
+  "ghcr.haguro.top": "https://ghcr.io",
+  "cloudsmith.haguro.top": "https://docker.cloudsmith.io",
+  "ecr.haguro.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.haguro.top": dockerHub,
 };
 
 function routeByHosts(host) {
